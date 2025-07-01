6fdd8d905d90b5b32b986aacc595e1679900ac4f
mediumpacer <lclancyau@gmail.com>
2025-07-01 13:54:17 +1000

    feat: improving log regex, refactored to use typescript, fixing ts related errors

---
+++ index.ts
@@ -0,0 +1,40 @@
+import { getRequestedLogValues } from './src/task.ts';
+
+
+/**
+ * Initializes the log parser and logs the requested values to the console
+ */
+const init = () => {
+  // change second parameter to true to include ties in the top values
+  const taskValues = getRequestedLogValues("./logs/programming-task-example-data.log", true);
+
+  if (!taskValues) {
+    console.log("No values returned from log file provided");
+    return;
+  }
+
+  console.log('-------------------------------------');
+  console.log('Number of unique IP addresses:', taskValues.uniqueIpAddresses);
+
+  console.log('-------------------------------------');
+  console.log('Top 3 most visited URLs:');
+  if (!taskValues.topUrls || taskValues.topUrls.length === 0) {
+    console.log('No URLs found in the log file.');
+  } else {
+    taskValues.topUrls.forEach((item) => {
+      console.log(`"${item[0]}":  (Count: ${item[1]})`);
+    });
+  }
+
+  console.log('-------------------------------------');
+  console.log('Top 3 most active IP addresses:');
+  if (!taskValues.topIpAddresses || taskValues.topIpAddresses.length === 0) {
+    console.log('No URLs found in the log file.');
+  } else {
+    taskValues.topIpAddresses.forEach((item) => {
+      console.log(`"${item[0]}":  (Count: ${item[1]})`);
+    });
+  }
+}
+
+init();
